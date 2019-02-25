ebcd947f7d59f4669d3ad8d35bb38896d371260d
Hebunzudoa <hd.heavensdoor@gmail.com>
2019-02-25 07:21:01 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count=0, i=0;
+  for (var i=0;i<preferences.length;i++) {
+    if (i===preferences[preferences[preferences[i]-1]-1]-1
+      &&preferences[i]!==preferences[preferences[i]-1]
+      &&preferences[preferences[preferences[i]-1]-1]!==preferences[preferences[i]-1]
+      &&preferences[i]!==preferences[preferences[preferences[i]-1]-1]) {
+      count+=1;
+      preferences[i]=0;
+      preferences[preferences[i]-1]=0;
+      preferences[preferences[preferences[i]-1]-1]=0; 
+    }
+  } 
+  return count;
 };
